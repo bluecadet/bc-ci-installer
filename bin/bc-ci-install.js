873a0947f99e0b5ee6de69f8d74a08ed42b4ca23
Pete Inge <pete.inge@gmail.com>
2019-11-08 13:16:58 -0500

    Updates
    tweaked args
    tweaked logging

--- bin/bc-ci-install.js
+++ bin/bc-ci-install.js
@@ -13,32 +13,39 @@ var GitHub = require('github-api');
 
 const creds = require('../lib/credentials.js');
 
+
+const yargs = require('yargs');
+
 const args = require('yargs')
+  .usage('Usage: $0 [options]')
   .alias('i', 'installer')
   .describe('i', "")
   .default('i', "bc-ci-for-pantheon") // @TODO: remove this after development
+  .choices('i', ['bc-ci-for-pantheon'])
   .alias('c', 'ci-version')
   .describe('c', "CI version or tag")
   .default('c', "1.0.0-alpha.1") // @TODO: remove this after development
-  .epilog('copyright 2019')
+  .boolean('t')
+  .alias('t', 'incTestConfig')
+  .describe('t', "Include Deafult Test Config Files. (This will destroy any changes you may have made in them)")
+  .default('t', false)
   .help('h')
   .alias('h', 'help')
+  .version()
   .alias('V', 'version')
   .count('verbose')
   .alias('v', 'verbose')
-  .default('v', 3) // @TODO: remove this after development
+  // .default('v', 3) // @TODO: remove this after development
   .describe('verbose', "Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug")
+  .epilog('Copyright 2019')
+  .wrap(null)
   .argv;
 
 let Logger = require('../lib/logger.js');
-let logger = new Logger(4);
+let logger = new Logger(args.verbose);
 let log = logger.log;
 
-console.log(args)
-
-return;
-
-var versionPattern = /([0-9]+)(\.([0-9x]+))?(\.([0-9x]+))?(-(([a-z]+)([.-]([0-9]+))?)?)?/;
+log(args, 3);
 
 // Configuration.
 let config = {
@@ -48,11 +55,35 @@ let config = {
     githubMachineToken: '',
     githubUsername: '',
     githubPassword: ''
-  }
+  },
+  versionPattern: /([0-9]+)(\.([0-9x]+))?(\.([0-9x]+))?(-(([a-z]+)([.-]([0-9]+))?)?)?/,
+  incTestConfig: args.incTestConfig,
+  verbose: args.verbose,
+  installer: args.i,
+  reqVersion: args.c
 };
 config.credsDir = config.baseTempDir + "/creds";
 config.credsFile = config.credsDir + '/creds.json';
 config.tmpRepoDir = config.baseTempDir + "/repo";
+
+log(config, 3);
+
+// Verify project.
+// Verify we are in a project directory...
+if (!fsx.existsSync('./web/sites/default/settings.php')) {
+  log(chalk.white.bgRed("\nNo `settings.php`, can't continue."));
+  log(chalk.white.bgRed("***Are you sure you are in the proper folder?***"));
+  return;
+}
+
+// Verify .projectconfig.js...
+if (!fsx.existsSync('./.projectconfig.js')) {
+  log(chalk.white.bgRed("\nNo `.projectconfig.js`, can't continue."));
+  log(chalk.white.bgRed("***Are you sure you are in the proper folder?***"));
+  return;
+}
+
+// Check default config files...
 
 creds.initCreds(config)
   .then((CredsResponse) => {
@@ -61,39 +92,38 @@ creds.initCreds(config)
 
     // Select project and version.
     // @TODO: remove defaults and actually figure this out.
-    config.installer = args.i;
+
     config.version = args.c;
-    config.installTestConfig = true;
 
     // Do IT!!
-    let installFile = './lib/installers/' + config.installer + '.js';
-    log(installFile);
-    if (fsx.existsSync(installFile)) {
-      log(chalk.green("Installer (" + config.installer + ") found."));
-      const installer = require('../lib/installers/' + config.installer + '.js');
+    let installerModule = '../lib/installers/' + config.installer + '.js';
+    log(installerModule, 2);
+
+    try {
+      const installer = require(installerModule);
       return installer.install(config);
     }
+    catch (err) {
+      if (err.code === 'MODULE_NOT_FOUND') {
+        log(chalk.red("Installer (" + config.installer + ") does not exist."));
+        return { status: "installer_not_found" };
+      }
 
-    log(chalk.red("Installer (" + config.installer + ") does not exist."));
-    return;
+      return { status: "unknown module require error" };
+    }
   })
+  // Handle Status.
   .then((response) => {
     if (response.status == "user_terminated") {
       log(chalk.red("User Terminated Installation."))
     }
+
+    if (response.status == "installer_not_found") {
+      log(chalk.red("Installer Not Found. Exiting Installation"))
+    }
   });
 
 
-
-
-
-
-// Verify project.
-// Verify we are in a project directory...
-// Verify .projectconfig.js...
-// Check default config files...
-
-// Check for saved Github creds.
 
 
 
